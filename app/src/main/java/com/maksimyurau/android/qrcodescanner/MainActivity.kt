988bb6f6cb833176cd64b7b4f3c5cfbc8f9d2d5d
MaksimYurau <2004softwaredeveloper@gmail.com>
2022-02-04 21:11:19 +0300

    Updated project. Updated MainActivity.kt. Updated strings.xml. Updated build.gradle.
    Signed-off-by: MaksimYurau <2004softwaredeveloper@gmail.com>

--- app/src/main/java/com/maksimyurau/android/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/maksimyurau/android/qrcodescanner/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.maksimyurau.android.qrcodescanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val qrButton = findViewById<ImageButton>(R.id.qrcode_scanner_ib)
+        qrButton.setOnClickListener {
+            val intentIntegrator = IntentIntegrator(this)
+            intentIntegrator.setDesiredBarcodeFormats(listOf(IntentIntegrator.QR_CODE))
+            intentIntegrator.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator.parseActivityResult(resultCode, data)
+        if (result != null) {
+            AlertDialog.Builder(this)
+                .setMessage("Would you like to go to ${result.contents}?")
+                .setPositiveButton(resources.getString(R.string.yes)) { _, i ->
+                    val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                    intent.putExtra(SearchManager.QUERY, result.contents)
+                    startActivity(intent)
+                }
+                .setNegativeButton(resources.getString(R.string.no)) { _, i -> }
+                .create()
+                .show()
+        }
     }
 }
